cd3a20298714a6651b70022b934919a65ed8f30a
anabra <peter.d.podlovics@gmail.com>
2019-11-30 21:47:54 +0100

    ES Alt: CreatedBy now handles named alternatives

--- grin/test/AbstractInterpretation/ExtendedSyntax/CreatedBySpec.hs
+++ grin/test/AbstractInterpretation/ExtendedSyntax/CreatedBySpec.hs
@@ -112,10 +112,10 @@ spec = do
             f x =
               z1 <- pure 0
               case x of
-                0@_1 ->
+                0 @ _1 ->
                   x0 <- pure (CInt z1)
                   pure x0
-                1@_2 ->
+                1 @ _2 ->
                   x1 <- pure (CBool z1)
                   pure x1
            |]
@@ -180,7 +180,7 @@ spec = do
               p0 <- store n0
               n1 <- pure (CCons z0 p0)
               case n1 of
-                (CCons x pxs)@_1 ->
+                (CCons x pxs) @ _1 ->
                   xs <- fetch pxs
                   pure 5
           |]
@@ -193,7 +193,7 @@ spec = do
                        , ("xs",  producerXS)
 
                        , ("z0",   emptyProducerSet)
-                       , ("_1",   emptyProducerSet)
+                       , ("_1",   producerN1)
                        ]
           producerN0 = mkProducerSet [(Tag C "Nil",  ["n0"])]
           producerN1 = mkProducerSet [(Tag C "Cons", ["n1"])]
@@ -206,13 +206,13 @@ spec = do
               z0 <- pure 0
               a0 <- f z0
               r0 <- case a0 of
-                (CInt c0)@_1  ->
+                (CInt c0)  @ _1  ->
                   b0 <- pure (CInt  z0)
                   pure b0
-                (CBool c1)@_2 ->
+                (CBool c1) @ _2 ->
                   b1 <- pure (CBool z0)
                   pure b1
-                (CNope c2)@_3 ->
+                (CNope c2) @ _3 ->
                   b2 <- pure (CNope z0)
                   pure b2
               pure r0
@@ -221,10 +221,10 @@ spec = do
             f x =
               z1 <- pure 0
               case x of
-                0@_4 ->
+                0 @ _4 ->
                   x0 <- pure (CInt  z0)
                   pure x0
-                1@_5 ->
+                1 @ _5 ->
                   x1 <- pure (CBool z0)
                   pure x1
           |]
@@ -243,8 +243,8 @@ spec = do
 
                        , ("z0", emptyProducerSet)
                        , ("z1", emptyProducerSet)
-                       , ("_1", emptyProducerSet)
-                       , ("_2", emptyProducerSet)
+                       , ("_1", producerX0)
+                       , ("_2", producerX1)
                        , ("_3", emptyProducerSet)
                        , ("_4", emptyProducerSet)
                        , ("_5", emptyProducerSet)
@@ -263,13 +263,13 @@ spec = do
               z0 <- pure 0
               a0 <- f z0
               r0 <- case a0 of
-                (CInt c0)@_1  ->
+                (CInt c0)  @ _1  ->
                   b0 <- f z0
                   pure b0
-                (CBool c1)@_2 ->
+                (CBool c1) @ _2 ->
                   b1 <- pure (CBool z0)
                   pure b1
-                (CNope c2)@_3 ->
+                (CNope c2) @ _3 ->
                   b2 <- pure (CNope z0)
                   pure b2
               pure r0
@@ -277,10 +277,10 @@ spec = do
             f x =
               z1 <- pure 0
               case x of
-                0@_4 ->
+                0 @ _4 ->
                   x0 <- pure (CInt z1)
                   pure x0
-                1@_5 ->
+                1 @ _5 ->
                   x1 <- pure (CBool z1)
                   pure x1
           |]
@@ -299,8 +299,8 @@ spec = do
 
                        , ("z0", emptyProducerSet)
                        , ("z1", emptyProducerSet)
-                       , ("_1", emptyProducerSet)
-                       , ("_2", emptyProducerSet)
+                       , ("_1", producerX0)
+                       , ("_2", producerX1)
                        , ("_3", emptyProducerSet)
                        , ("_4", emptyProducerSet)
                        , ("_5", emptyProducerSet)
@@ -320,13 +320,13 @@ spec = do
               a0 <- f z0
               a1 <- pure (CWord z0)
               r0 <- case a0 of
-                (CInt c0)@_1 ->
+                (CInt c0)  @ _1 ->
                   b0 <- g a0
                   pure b0
-                (CBool c1)@_2 ->
+                (CBool c1) @ _2 ->
                   b1 <- g a1
                   pure b1
-                (CNope c2)@_3 ->
+                (CNope c2) @ _3 ->
                   b2 <- pure (CNope z0)
                   pure b2
               pure r0
@@ -334,23 +334,23 @@ spec = do
             f x =
               z1 <- pure 1
               case x of
-                0@_4 ->
+                0 @ _4 ->
                   x0 <- pure (CInt  z1)
                   pure x0
-                1@_5 ->
+                1 @ _5 ->
                   x1 <- pure (CBool z1)
                   pure x1
 
             g y =
               z2 <- pure 1
               case y of
-                (CInt n)@_6 ->
+                (CInt n)  @ _6 ->
                   y0 <- pure (CInt  z2)
                   pure y0
-                (CBool b)@_7 ->
+                (CBool b) @ _7 ->
                   y1 <- pure (CBool z2)
                   pure y1
-                (CWord w)@_8 ->
+                (CWord w) @ _8 ->
                   y2 <- pure (CWord z2)
                   pure y2
           |]
@@ -379,14 +379,14 @@ spec = do
                        , ("z0", emptyProducerSet)
                        , ("z1", emptyProducerSet)
                        , ("z2", emptyProducerSet)
-                       , ("_1", emptyProducerSet)
-                       , ("_2", emptyProducerSet)
+                       , ("_1", producerX0)
+                       , ("_2", producerX1)
                        , ("_3", emptyProducerSet)
                        , ("_4", emptyProducerSet)
                        , ("_5", emptyProducerSet)
-                       , ("_6", emptyProducerSet)
+                       , ("_6", producerX0)
                        , ("_7", emptyProducerSet)
-                       , ("_8", emptyProducerSet)
+                       , ("_8", producerA1)
                        ]
           producerX0 = mkProducerSet [(Tag C "Int",  ["x0"])]
           producerX1 = mkProducerSet [(Tag C "Bool", ["x1"])]
@@ -597,8 +597,8 @@ spec = do
 
             , ("z0", tySetFromTypes [T_Int64])
             , ("z1", tySetFromNodeSet $ mkNodeSet [ (cInt,  [ [T_Int64] ]) ])
-            , ("_1", tySetFromTypes [])
-            , ("_2", tySetFromTypes [])
+            , ("_1", tySetFromTypes [T_Int64])
+            , ("_2", tySetFromTypes [T_Int64])
             , ("_3", tySetFromNodeSet $ mkNodeSet [ (cNode, [ [T_UnspecifiedLocation] ]) ])
             ]
           unspecLocExpectedFunctions = M.singleton "grinMain" (mkSimpleMain T_Unit)
